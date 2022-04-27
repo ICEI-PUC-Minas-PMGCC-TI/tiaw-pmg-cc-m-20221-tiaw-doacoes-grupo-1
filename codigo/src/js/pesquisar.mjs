b159a368fad88a966b89898963cde3f13f66cdee
MrNaru300 <bruno.santi.oli@gmail.com>
2022-04-27 18:06:02 -0300

    feat: descrition e url-image

--- codigo/src/js/pesquisar.mjs
+++ codigo/src/js/pesquisar.mjs
@@ -1,8 +1,10 @@
 let options = {
-    valueNames: ['name', 'region', 'date'],
+    valueNames: ['name', 'region', 'date', 'description', 'url-image'],
     item: values => {
         return `<li>
             <h3>${values.name}</h3>
+            <p>${values.description}</p>
+            <img src='${values['url-image']}'/>
             <p>${values.region}</p>
             <p>${values.date}</p>
         </li>`
@@ -10,12 +12,18 @@ let options = {
 }
 
 let values = [
- {name: 'Deslizamento em Petrópolis', date: '15/02/2022', region: 'Petropolis - RJ'}
+    {
+        name: 'Deslizamento em Petrópolis',
+        date: '15/02/2022',
+        'url-image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg/280px-18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg',
+        region: 'Petrópolis - RJ', 
+        description: 'As enchentes e deslizamentos de terra em Petrópolis, cidade localizada na Região Serrana no estado do Rio de Janeiro, Brasil, ocorreram no fim da tarde e na noite de 15 de fevereiro de 2022. Foram 775 deslizamentos de terra em toda a cidade, além de diversas ruas alagadas.'
+    }
 ]
 
 let campaigns = new List("campaign-results", options, values)
 
 document.querySelector("#search-campaign > input").addEventListener('input', e => {
-    campaigns.fuzzySearch(e.target.value);
+    campaigns.fuzzySearch(e.target.value, ['name', 'region', 'date', 'description']);
 })
 
