58d4ee2ddea96bd842ae424466c49659ffec31d0
MrNaru300 <bruno.santi.oli@gmail.com>
2022-05-01 18:04:48 -0300

    feat: categories

--- codigo/src/js/pesquisar.js
+++ codigo/src/js/pesquisar.js
@@ -1,6 +1,6 @@
 
 let options = {
-    valueNames: ['name', 'region', 'date', 'description', 'url-image'],
+    valueNames: ['name', 'region', 'date', 'description', 'url-image', 'categories'],
     item: values => {
         return `<li class="campaign-item">
             <h3>${values.name}</h3>
@@ -8,6 +8,7 @@ let options = {
             <img src='${values['url-image']}'/>
             <p>${values.region}</p>
             <p>${values.date}</p>
+            <p>${values.categories}</p>
         </li>`
     } 
 }
@@ -18,13 +19,22 @@ let values = [
         date: '15/02/2022',
         'url-image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg/280px-18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg',
         region: 'Petrópolis - RJ', 
-        description: 'As enchentes e deslizamentos de terra em Petrópolis, cidade localizada na Região Serrana no estado do Rio de Janeiro, Brasil, ocorreram no fim da tarde e na noite de 15 de fevereiro de 2022. Foram 775 deslizamentos de terra em toda a cidade, além de diversas ruas alagadas.'
+        description: 'As enchentes e deslizamentos de terra em Petrópolis, cidade localizada na Região Serrana no estado do Rio de Janeiro, Brasil, ocorreram no fim da tarde e na noite de 15 de fevereiro de 2022. Foram 775 deslizamentos de terra em toda a cidade, além de diversas ruas alagadas.',
+        categories: ['deslizamento', 'inundação']
+    },
+    {
+        name: 'abc def',
+        date: '15/02/2123',
+        'url-image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg/280px-18_02_2022_Sobrevoo_e_reuni%C3%A3o_de_trabalho_sobre_medidas_emergenciais_do_Governo_Federal_para_a_cidade_de_Petr%C3%B3polis_%2851889351218%29.jpg',
+        region: 'Belo Horizonte - MG', 
+        description: 'Teste',
+        categories: ['deslizamento', 'inundação']
     }
 ]
 
 let campaigns = new List("campaign-results", options, values)
 
 document.querySelector("#search-campaign > input").addEventListener('input', e => {
-    campaigns.search(e.target.value, ['name', 'region', 'date', 'description']);
+    campaigns.fuzzySearch(e.target.value, ['name', 'region', 'date', 'description', 'categories']);
 })
 
