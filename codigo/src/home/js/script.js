22b00154f52111113cdcf57fda8870da57660100
Fernando Campos Silva Dal' Maria <fernando.csm123@gmail.com>
2022-05-13 18:13:27 -0300

    feat: testes finalizados

--- codigo/src/home/js/script.js
+++ codigo/src/home/js/script.js
@@ -1,28 +1,40 @@
 /* JS ORDER GRID */
-$("#menu").click(function () {
-   $(".overlay").toggleClass("open");
-   $(".superlay").toggleClass("open");
-});
+const menu = document.querySelector('#menu');
+const overlay = document.querySelector(".overlay");
+const superlay = document.querySelector(".superlay");
 
-$(".cross").click(function () {
-   $(".overlay").addClass("open");
-   $(".superlay").toggleClass("open");
-});
+menu.onclick = () => {
+   const visibility = menu.getAttribute('aria-expanded');
+   if(visibility === 'false') menu.setAttribute('aria-expanded', true);
+   else if(visibility === 'true') menu.setAttribute('aria-expanded', false);
+   overlay.classList.toggle("open");
+   superlay.classList.toggle("open");
+}
 
 /* FORM-NOTIFICATION */
-
 const form = document.querySelector('#formNotification');
 const nome = document.querySelector('#name');
 const gmail = document.querySelector('#email');
 const mensagem = document.querySelector('#message');
 const btnForm = document.querySelector('#btnFormNotify');
 
+const sum = (a = 0, b = 0) => ((typeof a) == 'number' && (typeof b) == 'number') ? a+b : 0; //exemplo
+
 form.onsubmit = (e) => {
    //e.preventDefault();
+   // const posicao = () => {
+   //    let posi = navigator.geolocation.getCurrentPosition(position);
+   //    function position(pos) {
+         
+   //    }
+   // }
+   // posicao()
    const identificador = {
       nome: nome.value,
       email: gmail.value,
-      mensagem: mensagem.value
+      mensagem: mensagem.value,
+      data: new Date()
+      // posicionamento: positioning
    }
    localStorage.setItem('identificador', JSON.stringify(identificador));
 }
@@ -41,7 +53,12 @@ form.onclick = (e) => {
 }
 
 mensagem.oninput = () => {
-   if(mensagem.value.length > 300) {
+   if (mensagem.value.length > 300) {
       mensagem.value = mensagem.value.substr(0, mensagem.value.length - 1);
    }
 }
+
+module.exports = {
+   sum,
+}
+
