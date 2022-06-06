a4ecb4441a881f97af4bacbdf46d4f8de6bba0c2
Rafael Fleury <rfbcol@gmail.com>
2022-06-06 17:18:57 -0300

    informations-sprint_3

--- codigo/src/informations/js/script.js
+++ codigo/src/informations/js/script.js
@@ -71,3 +71,52 @@ $(document).ready(function() {
     }, 1900);
   });
 });
+
+// Dropdown Menu
+var dropdown = document.querySelectorAll(".dropdown");
+var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+dropdownArray.forEach(function (el) {
+  var button = el.querySelector('a[data-toggle="dropdown"]'),
+    menu = el.querySelector(".dropdown-menu"),
+    arrow = button.querySelector("i.icon-arrow");
+
+  button.onclick = function (event) {
+    if (!menu.hasClass("show")) {
+      menu.classList.add("show");
+      menu.classList.remove("hide");
+      arrow.classList.add("open");
+      arrow.classList.remove("close");
+      event.preventDefault();
+    } else {
+      menu.classList.remove("show");
+      menu.classList.add("hide");
+      arrow.classList.remove("open");
+      arrow.classList.add("close");
+      event.preventDefault();
+    }
+  };
+});
+
+Element.prototype.hasClass = function (className) {
+  return (
+    this.className &&
+    new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className)
+  );
+};
+
+function atlas(state) {
+  state = state.substr(0, 2);
+
+  console.log(state);
+
+  atlas1 = document.getElementById('atlas-IF');
+
+  atlas1.src = `/src/pdfs/Atlas/${state}.pdf`;
+
+  console.log(`Texto com state: /src/pdfs/Atlas/${state}.pdf`);
+
+  console.log(`Atlas1: ${atlas1.src}`);
+
+  //document.getElementById('atlas-IF').scr = "/src/pdfs/AM.pdf";
+
+};
